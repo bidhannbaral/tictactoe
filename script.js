c0b45c006c5eccf1be5f49e2958d4c89c0a31036
Bidhan Baral <bbaral2062@gmail.com>
2025-02-21 20:11:51 -0500

    Have to check for win

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
 const cells = document.querySelectorAll(".cell");
 const start = document.querySelector(".start");
 const text = document.querySelector(".text");
+let player1 = true;
+let gameActive = false;
 
 start.addEventListener("click", () => {
-    startGame();
+    start.textContent = "Reset Game";
+    text.textContent = "Player 1's turn";
+    player1 = true;
+    gameActive = true;
+        cells.forEach((cell) => {
+            cell.textContent = "";
+        });
 });
 
-function startGame() {
-
-    
-    cells.forEach((cell) => {
-        cell.addEventListener("click", () => {
-            cell.innerHTML = `<p id="inner-text">X</p>`;
-        });
+cells.forEach((cell) => {
+    cell.addEventListener("click", () => {
+        if (gameActive) {
+            playMove(cell);
+        }
     });
+});
+
+function playMove(cell) {
+    if (cell.textContent == "") {
+        if (player1) {
+            console.log("player 1");
+            cell.innerHTML = `<p id="inner-text">X</p>`;
+            text.textContent = "Player 2's turn";
+        } else {
+            console.log("player 2");
+            cell.innerHTML = `<p id="inner-text">O</p>`;
+            text.textContent = "Player 1's turn";
+        }
+    }
+    player1 = !player1;
+}
+
+function checkWinner() {
+    
 }
